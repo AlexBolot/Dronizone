0ca8f42a229fbc8e18384a18e88e03a0c2a19631
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2019-09-22 22:51:14 +0200

    Adding empty Drone Service module Working on #5 (hello world architecture)

---
+++ drone/src/main/java/fr/unice/polytech/codemara/App.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.codemara;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ drone/src/test/java/fr/unice/polytech/codemara/AppTest.java
@@ -0,0 +1,12 @@
+package fr.unice.polytech.codemara;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void basicTest() {
+        Assert.assertTrue(true);
+    }
+}
