7679051c49f890ec29b2bcc3aba23aff70806a48
Gregoire Peltier <pg501842@etu.unice.fr>
2019-09-22 21:03:29 +0200

    Testing Travis failing and branch listening

--- src/test/java/fr/unice/polytech/codemara/AppTest.java
+++ src/test/java/fr/unice/polytech/codemara/AppTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.fail;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTest {
@@ -14,4 +16,8 @@ public class AppTest {
     public void contextLoads() {
     }
 
+    @Test
+    public void travisShouldFail() {
+        fail("Failing Travis ");
+    }
 }
