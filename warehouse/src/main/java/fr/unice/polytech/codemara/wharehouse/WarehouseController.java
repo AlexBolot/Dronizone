306e1b48bd56b24228b0ffe83c614a1e432a7c29
Gregoire Peltier <pg501842@etu.unice.fr>
2019-09-23 11:57:39 +0200

    #5 Added a basic hello world warehouse service

---
+++ warehouse/src/main/java/fr/unice/polytech/codemara/wharehouse/WarehouseController.java
@@ -0,0 +1,19 @@
+package fr.unice.polytech.codemara.wharehouse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/warehouse", produces = "application/json")
+public class WarehouseController {
+    @GetMapping("/{hello}")
+    public String order_ping(@PathVariable("hello") String hello){
+        if (hello.equals("hello")){
+            return "World";
+
+        }
+        return "Hello";
+    }
+}
