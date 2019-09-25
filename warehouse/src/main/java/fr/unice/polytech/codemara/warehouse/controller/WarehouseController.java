03451046f61be7bacc510c195710ab3a3879d579
Prune Pillone <prune6@gmail.com>
2019-09-25 16:17:04 +0200

    Add hello world

--- warehouse/src/main/java/fr/unice/polytech/codemara/warehouse/controller/WarehouseController.java
+++ warehouse/src/main/java/fr/unice/polytech/codemara/warehouse/controller/WarehouseController.java
@@ -1,19 +1,28 @@
-package fr.unice.polytech.codemara.warehouse;
+package fr.unice.polytech.codemara.warehouse.controller;
 
+import fr.unice.polytech.codemara.warehouse.entities.Warehouse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
 @RequestMapping(path = "/warehouse", produces = "application/json")
 public class WarehouseController {
     @GetMapping("/{hello}")
-    public String order_ping(@PathVariable("hello") String hello){
-        if (hello.equals("hello")){
+
+    public String order_ping(@PathVariable("hello") String hello) {
+        if (hello.equals("hello")) {
             return "World";
 
         }
         return "Hello";
+    }
+
+    @RequestMapping(method = GET, path = "/pending")
+    public String getPendings() {
+        return "Warehouse.getPendings().toString()";
     }
 }
---
+++ warehouse/src/main/java/fr/unice/polytech/codemara/warehouse/entities/Stock.java
@@ -0,0 +1,4 @@
+package fr.unice.polytech.codemara.warehouse.entities;
+
+public class Stock {
+}
