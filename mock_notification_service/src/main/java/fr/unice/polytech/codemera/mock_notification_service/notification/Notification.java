3b0673bd5c70447615980f10ffe9b9ec51c4cce4
Gregoire Peltier <pg501842@etu.unice.fr>
2019-10-02 18:38:21 +0200

    CDMR-8 The notification mock service can now listen for incoming http query for a notification dispatch

---
+++ mock_notification_service/src/main/java/fr/unice/polytech/codemera/mock_notification_service/notification/Notification.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.codemera.mock_notification_service.notification;
+
+public class Notification {
+    public long target_id;
+    public String payload;
+}
+
---
+++ mock_notification_service/src/main/java/fr/unice/polytech/codemera/mock_notification_service/notification/OrderNotification.java
@@ -0,0 +1,9 @@
+package fr.unice.polytech.codemera.mock_notification_service.notification;
+
+/**
+ * Serves as a data object for an incoming order notification
+ * If changed the readme.md should be edited as well
+ */
+public class OrderNotification extends CustomerNotification{
+    public String orderName;
+}
