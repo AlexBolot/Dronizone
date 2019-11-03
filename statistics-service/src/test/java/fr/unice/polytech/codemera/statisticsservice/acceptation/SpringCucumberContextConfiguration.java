c88a5928bc80493602a069f0bfc030336e4fd8f1
Gregoire Peltier <pg501842@etu.unice.fr>
2019-11-03 14:59:59 +0100

    CDMR-26 Statistics service sonar and test cleanup

--- statistics-service/src/test/java/fr/unice/polytech/codemera/statisticsservice/acceptation/SpringCucumberContextConfiguration.java
+++ statistics-service/src/test/java/fr/unice/polytech/codemera/statisticsservice/acceptation/SpringCucumberContextConfiguration.java
@@ -1,11 +1,13 @@
 package fr.unice.polytech.codemera.statisticsservice.acceptation;
 
 import io.cucumber.java.Before;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
-@AutoConfigureMockMvc
+@Configuration
 public class SpringCucumberContextConfiguration {
     @Before
     public void setup_cucumber_spring_context() {
